c8754ee3a2f7c32763566992488a350d1a122dff
Flor <fcflorencia@hotmail.es>
2020-11-30 12:38:28 +0000

    function setdate

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    setDate();
+})
+
+function setDate() {
+    const dateInput = [...document.forms[0].children][3];
+    const date = new Date();
+    const today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;    
+    dateInput.setAttribute('min', today);
+    dateInput.value = today;
+}
