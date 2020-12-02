7ca14ae2dc04708c0054f693d0b794245e146806
Flor <fcflorencia@hotmail.es>
2020-12-02 13:31:40 +0000

    added template room

--- js/index.js
+++ js/index.js
@@ -15,13 +15,36 @@ function setDate () {
 
 function createOptionsNights () {
     const nightSelect = formElements[5];
-    const nights = 14;
-    for (let i = 1; i <= nights; i++) {
+    const maxNights = 14;
+    for (let i = 1; i <= maxNights; i++) {
         const option = document.createElement('option');
         const value = (i === 1) ? `${i} Night` : `${i} Nights`;
         option.value = value;
         const text = document.createTextNode(value);
         option.appendChild(text);
         nightSelect.appendChild(option);
+    }
+}
+
+const roomsInput = document.querySelector('input[name="rooms"');
+roomsInput.addEventListener('click', () => {
+    createRoomElement();
+});
+
+function createRoomElement () {
+    if ('content' in document.createElement('template')) {
+        const newRoomElement = document.querySelector('#new-room');
+        if (newRoomElement.hasChildNodes) {
+            const children = [...newRoomElement.children];
+            children.forEach(child => {
+                child.remove();
+            });
+        }
+        const roomTemplate = document.querySelector('#room');
+        const clone = document.importNode(roomTemplate.content, true);
+        newRoomElement.appendChild(clone);
+    } else {
+        // Buscar otra manera de añadir habitaciones
+        // porque el elemento template no está soportado.
     }
 }
